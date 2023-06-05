8174d6bf30e5dd5c135e6d5d0cca72b85aa0cd63
JudahSan <baraka.judah@yahoo.com>
2023-06-05 20:27:34 +0300

    extra debug for stuff module

--- rustAndAssembly/rust_assemble/src/binary_adder_test.rs
+++ rustAndAssembly/rust_assemble/src/binary_adder_test.rs
@@ -1,16 +1,20 @@
-use crate::binary_adder;
+#[cfg(test)]
+mod tests {
+    use crate::binary_adder;
+    use assert_eq;
 
-  #[test]
-  fn test_binary_adder() {
-    // Test case 1
-    let sum = binary_adder(0b101, 0b110);
-    assert_eq!(sum, 0b1011);
+    #[test]
+    fn test_binary_adder() {
+        // Test case 1
+        let sum = binary_adder(0b101, 0b110);
+        assert_eq!(sum, 0b1011);
 
-    // Test case 2
-    let sum = binary_adder(0b10101, 0b11010);
-    assert_eq!(sum, 0b11011);
+        // Test case 2
+        let sum = binary_adder(0b10101, 0b11010);
+        assert_eq!(sum, 0b101111);
 
-    // Test case 3
-    let sum = binary_adder(0b1111, 0b1111);
-    assert_eq!(sum, 0b01110); // 0b11110 is tge correct output
-  }
+        // Test case 3
+        let sum = binary_adder(0b1111, 0b1111);
+        assert_eq!(sum, 0b11110); // Corrected expected output
+    }
+}
